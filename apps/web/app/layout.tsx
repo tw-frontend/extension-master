aabc0cf8811b511d5fbcc357efa17800e22ae120
Iman <aaarjmandnia@gmail.com>
2026-09-09 16:11:42 +0330

    feat: trying to redeploy

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -29,6 +29,7 @@ const themeScript = `
 })();
 `;
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
